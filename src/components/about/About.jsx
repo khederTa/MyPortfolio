a92369edfa2488493d324f0c3fccb5eef42e16d8
Kheder <62546731+khederTa@users.noreply.github.com>
2023-01-28 21:58:08 +0300

    Update About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -33,7 +33,7 @@ const About = () => {
             <article className='about__card'>
               <VscFolderLibrary className='about__icon' />
               <h5>Projects</h5>
-              <small>30+ Completed</small>
+              <small>20+ Completed</small>
             </article>
           </div>
 
