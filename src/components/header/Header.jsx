351948b1086b198eb95e5cb9ea6cff0ebc9a0f3b
Kheder <62546731+khederTa@users.noreply.github.com>
2023-01-28 21:32:56 +0300

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Kheder Taleb</h1>
-        <h5 className="text-light">Software Engineer</h5>
+        <h5 className="text-light">Full Stack Web Developer</h5>
         <CTA />
         <HeaderSocials />
 
