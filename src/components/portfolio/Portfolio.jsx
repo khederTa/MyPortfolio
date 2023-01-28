92428865facbc6d2436069fef14b222386d73299
Kheder <62546731+khederTa@users.noreply.github.com>
2023-01-29 02:00:54 +0300

    Update Portfolio.jsx

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -10,37 +10,37 @@ import IMG6 from '../../assets/portfolio6.jpg'
 const data = [
   {
     id: 1,
-    image: IMG1,
-    title: 'title for the project',
-    github: 'https://github.com',
+    image: "https://th.bing.com/th/id/OIP.bBveFtIuwGY-bdCqCMmZtQHaE8?pid=ImgDet&rs=1",
+    title: 'TED web scrapng and classification video by the video by its transcript',
+    github: 'https://github.com/khederTa/Ted-Web-Scraping-And-Classification-Videos',
     demo: 'https://github.com'
   },
   {
     id: 2,
-    image: IMG2,
-    title: 'title for the project',
-    github: 'https://github.com',
+    image: "https://th.bing.com/th/id/OIP.7J7nVH4LeNwwBbSbw44UFQHaFj?w=235&h=180&c=7&r=0&o=5&dpr=1.1&pid=1.7",
+    title: 'Sign language recognition using deep learning ASL',
+    github: 'https://github.com/khederTa/Sign-language-recognition-using-deep-learning-ASL',
     demo: 'https://github.com'
   },
   {
     id: 3,
-    image: IMG3,
-    title: 'title for the project',
-    github: 'https://github.com',
+    image: "https://th.bing.com/th/id/OIP.lIOSK0S3LcDq9DzCo4fjyQHaEE?w=287&h=180&c=7&r=0&o=5&dpr=1.1&pid=1.7",
+    title: "Logistic regression classifier to recognize cats",
+    github: 'https://github.com/khederTa/logistic-regression-classifier-to-recognize-cats',
     demo: 'https://github.com'
   },
   {
     id: 4,
-    image: IMG4,
-    title: 'title for the project',
+    image: 'https://th.bing.com/th?id=OIP.tDu-vQE9VA_Cp3wYMSw_jwHaFj&w=288&h=216&c=8&rs=1&qlt=90&o=6&dpr=1.1&pid=3.1&rm=2',
+    title: 'Book online store MERN stack',
     github: 'https://github.com',
     demo: 'https://github.com'
   },
   {
     id: 5,
-    image: IMG5,
-    title: 'title for the project',
-    github: 'https://github.com',
+    image: "https://th.bing.com/th/id/OIP.aQjlwxDUtFwQ_hAOLDbBFwHaE8?w=268&h=180&c=7&r=0&o=5&dpr=1.1&pid=1.7",
+    title: 'Food delivery system MVC',
+    github: 'https://github.com/khederTa/Food_delivery_system',
     demo: 'https://github.com'
   },
   {
@@ -61,7 +61,7 @@ const Portfolio = () => {
 
       <div className="container portfolio__container">
         {
-          data.map(({ id, image, title, github, demo }) => {
+          data.map(({ id, image, title, github}) => {
             return (
               <article key={id} className="portfolio__item">
                 <div className="portfolio__item-image">
@@ -70,7 +70,7 @@ const Portfolio = () => {
                 <h3>This is a portfolio item title</h3>
                 <div className="portfolio__item-cta">
                   <a href={github} className='btn' target='_blank' rel='noreferrer noopener'>Github</a>
-                  <a href={demo} className='btn btn-primary' target='_blank' rel='noreferrer noopener'>Live Demo</a>
+                 {/* <a href={demo} className='btn btn-primary' target='_blank' rel='noreferrer noopener'>Live Demo</a>*/}
                 </div>
               </article>
             )
