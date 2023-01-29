742b464d24ac2e79a8eb21bd34f76e7c6af7ba78
Kheder <62546731+khederTa@users.noreply.github.com>
2023-01-29 11:48:48 +0300

    Update Portfolio.jsx

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -11,42 +11,42 @@ const data = [
   {
     id: 1,
     image: "https://th.bing.com/th/id/OIP.bBveFtIuwGY-bdCqCMmZtQHaE8?pid=ImgDet&rs=1",
-    title: 'TED web scrapng and classification video by the video by its transcript',
+    title: 'TED Web Scraping And Classification Video By Its Transcript',
     github: 'https://github.com/khederTa/Ted-Web-Scraping-And-Classification-Videos',
     demo: 'https://github.com'
   },
   {
     id: 2,
     image: "https://th.bing.com/th/id/OIP.7J7nVH4LeNwwBbSbw44UFQHaFj?w=235&h=180&c=7&r=0&o=5&dpr=1.1&pid=1.7",
-    title: 'Sign language recognition using deep learning ASL',
+    title: 'Sign Language Recognition Using Deep Learning ASL',
     github: 'https://github.com/khederTa/Sign-language-recognition-using-deep-learning-ASL',
     demo: 'https://github.com'
   },
   {
     id: 3,
     image: "https://th.bing.com/th/id/OIP.lIOSK0S3LcDq9DzCo4fjyQHaEE?w=287&h=180&c=7&r=0&o=5&dpr=1.1&pid=1.7",
-    title: "Logistic regression classifier to recognize cats",
+    title: "Logistic Regression Classifier To Recognize Cats",
     github: 'https://github.com/khederTa/logistic-regression-classifier-to-recognize-cats',
     demo: 'https://github.com'
   },
   {
     id: 4,
     image: 'https://th.bing.com/th?id=OIP.tDu-vQE9VA_Cp3wYMSw_jwHaFj&w=288&h=216&c=8&rs=1&qlt=90&o=6&dpr=1.1&pid=3.1&rm=2',
-    title: 'Book online store MERN stack',
+    title: 'Book Online Store MERN Stack',
     github: 'https://github.comkhederTa/book-online-store-MERN-stack',
     demo: 'https://github.com'
   },
   {
     id: 5,
     image: "https://th.bing.com/th/id/OIP.aQjlwxDUtFwQ_hAOLDbBFwHaE8?w=268&h=180&c=7&r=0&o=5&dpr=1.1&pid=1.7",
-    title: 'Food delivery system MVC',
+    title: 'Food Delivery System MVC',
     github: 'https://github.com/khederTa/Food_delivery_system',
     demo: 'https://github.com'
   },
   {
     id: 6,
     image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTu0hKafRVjPI0tPkEPVAhy-krdK_BaJE9eUlKP8Qo9&s",
-    title: 'E-commerce MERN stack',
+    title: 'E-Commerce MERN Stack',
     github: 'https://github.com',
     demo: 'https://github.com'
   }
