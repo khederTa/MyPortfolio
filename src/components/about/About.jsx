69958f32f217bf9f2687568b438c40b44b66994f
Kheder Taleb <62546731+khederTa@users.noreply.github.com>
2023-05-30 12:17:58 +0200

    Update About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -39,7 +39,7 @@ const About = () => {
 
           <p> 
              
-As a software engineer with extensive experience, I am passionate about solving complex problems using the latest technologies. My expertise lies in developing web applications, and I am proficient in several programming languages and frameworks such as HTML5, CSS3, JavaScript (ES6, ES7), React.js, Node.js, and Python. In addition, I have a deep understanding of machine learning and deep learning technologies, including TensorFlow and Keras.
+As a software engineer with extensive experience, I am passionate about solving complex problems using the latest technologies. My expertise lies in developing web applications, and I am proficient in several programming languages and frameworks such as HTML5, CSS3, JavaScript (ES6, ES7), Java, React.js, Node.js, and Python. In addition, I have a deep understanding of machine learning and deep learning technologies, including TensorFlow and Keras.
 Throughout my career, I have worked with diverse teams of developers, where I have developed excellent communication and problem-solving skills that have enabled me to drive successful project outcomes. I believe in staying up-to-date with the latest trends in software engineering and machine learning, and I am always looking for ways to enhance my knowledge to deliver innovative and cutting-edge solutions.
 Overall, I am an enthusiastic and dedicated software engineer who is ready to take on new challenges, learn new technologies, and deliver innovative solutions.
 
