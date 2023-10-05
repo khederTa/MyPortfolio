23b810b98f3c8f4df3c403eabcdc38d977c04b55
Kheder Taleb <62546731+khederTa@users.noreply.github.com>
2023-10-05 17:03:22 +0100

    Update CTA.jsx
    update resume link

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -3,7 +3,7 @@ import CV from '../../assets/resume.pdf'
 const CTA = () => {
   return (
     <div className='cta'>
-      <a href={CV} download className='btn'>Download CV</a>
+      <a href='https://docs.google.com/document/d/1-35IUWsSKirALQAE9MopNMS3gzULakkBueeoT7hbelY/edit?usp=sharing' download className='btn'>Download CV</a>
       <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
   )
